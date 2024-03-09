0a881e242f7b1c1d94f4543902888355dc8c48e4
Daniel Chang <changsongtao07@163.com>
2024-03-09 23:09:12 +0800

    modify plot.

--- running_test.py
+++ running_test.py
@@ -8,21 +8,30 @@ from lang_sam import LangSAM
 import torch
 from torchvision.utils import draw_bounding_boxes
 from torchvision.utils import draw_segmentation_masks
-
-print("Enter LangSAM")
-
-model = LangSAM()
-image_pil = Image.open("./assets/car.jpeg").convert("RGB")
-text_prompt = "person"
-masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
+import matplotlib.cm as cm
 
 def draw_image(image, masks, boxes, labels, alpha=0.4):
     image = torch.from_numpy(image).permute(2, 0, 1)
-    if len(boxes) > 0:
-        image = draw_bounding_boxes(image, boxes, colors=['red'] * len(boxes), labels=labels, width=5)
+
+    # Generate a colormap with number_of_objects different colors
+    cmap = cm.get_cmap('tab10')
+    label_colors = [cmap(i, bytes=True)[:3] for i in range(len(labels))]  # Discard alpha channel
+    
     if len(masks) > 0:
-        image = draw_segmentation_masks(image, masks=masks, colors=['cyan'] * len(masks), alpha=alpha)
+        # Use the generated colors for segmentation masks
+        image = draw_segmentation_masks(image, masks=masks, colors=label_colors, alpha=alpha)
+    
+    if len(boxes) > 0:
+        # Use the generated colors for bounding boxes
+        image = draw_bounding_boxes(image, boxes, colors=label_colors, labels=labels, width=5)
+    
     return image.numpy().transpose(1, 2, 0)
+
+model = LangSAM()
+
+image_pil = Image.open("./assets/car.jpeg").convert("RGB")
+text_prompt = "person"
+masks, boxes, phrases, logits = model.predict(image_pil, text_prompt)
 
 # Draw the image with bounding boxes and masks
 image_with_boxes = draw_image(np.array(image_pil), masks, boxes, phrases)
